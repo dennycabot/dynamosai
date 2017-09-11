7d1cb1619a98c14832ab67c581486ef8352bc8f2
Karthik Syamji <karthik.syamji@cabotsolutions.com>
2017-09-11 13:21:38 +0530

    beacon controller added

---
+++ DynamosAI/BeaconsViewController.swift
@@ -0,0 +1,74 @@
+//
+//  BeaconsViewController.swift
+//  DynamosAI
+//
+//  Created by Karthik on 9/11/17.
+//  Copyright © 2017 Cabot. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+
+class BeaconsViewController: UIViewController, CLLocationManagerDelegate {
+
+    var locationManager: CLLocationManager!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+                if CLLocationManager.isRangingAvailable() {
+                    startScanning()
+                }
+            }
+        }
+    }
+    
+    func startScanning() {
+        let beaconRegion = CLBeaconRegion(proximityUUID: NSUUID(uuidString: "f7826da6-4fa2-4e98-8024-bc5b71e0893e")! as UUID, identifier: "iBeacon I5Kl")
+        locationManager.startMonitoring(for: beaconRegion)
+        locationManager.startRangingBeacons(in: beaconRegion)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
+        if beacons.count > 0 {
+            updateDistance(beacons[0].proximity)
+            print(beacons.count)
+        } else {
+            updateDistance(.unknown)
+        }
+    }
+    
+    func updateDistance(_ distance: CLProximity) {
+        UIView.animate(withDuration: 0.8) {
+            switch distance {
+            case .unknown:
+                self.view.backgroundColor = UIColor.gray
+                
+            case .far:
+                self.view.backgroundColor = UIColor.blue
+                
+            case .near:
+                self.view.backgroundColor = UIColor.orange
+                
+            case .immediate:
+                self.view.backgroundColor = UIColor.red
+            }
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
